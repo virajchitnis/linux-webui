48cdeec7df9fefb5f9cb416aee2364b2e7c132c0
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-15 16:59:02 -0400

    Added missing css file to index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="design.css">
+		<link rel="stylesheet" type="text/css" href="index.css">
 		<title>linux-webui - Linux Server Control Panel</title>
 	</head>
     <body>
