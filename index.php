ba80166f2bb0abb7cec49c024b50148e41e1dd49
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-15 15:42:06 -0400

    Added index.php and design.css with the base UI design

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="design.css">
+	</head>
+    <body>
+        <div class="wrapper">
+        	<div class="header">
+        		<table class="header_table">
+        			<tr>
+        				<td id="header_table_data_left">Test text</td>
+        				<td id="header_table_data_right">Test text</td>
+        			</tr>
+        		</table>
+        	</div>
+            <div class="push"></div>
+        </div>
+        <div class="footer">
+            <p>&copy; 2014 Viraj Chitnis</p>
+        </div>
+    </body>
+</html>
