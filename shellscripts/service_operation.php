e50d38de8b9859582ed1f80fb1111568282090be
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-15 19:57:01 -0400

    Completed service_operation.php

--- shellscripts/service_operation.php
+++ shellscripts/service_operation.php
@@ -1,5 +1,9 @@
 <?php
-	if () {
+	if ((isset($_POST['service'])) && (isset($_POST['operand']))) {
+		$service = $_POST['service'];
+		$operand = $_POST['operand'];
 		exec("sudo service ".$service." ".$operand);
 	}
+	
+	header('Location ../services.php');
 ?>
