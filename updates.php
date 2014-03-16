5d7ea544c7a12899eeb4727a6cad69513036db3e
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 09:44:06 -0400

    Check updates command added

--- updates.php
+++ updates.php
@@ -7,8 +7,9 @@
 				document.getElementById('framedisplay').contentWindow.location.reload();
 			}
 		</script>
+		<?php exec("nohup emerge -pv --update --deep --with-bdeps=y --newuse @world >/tmp/linux-webui_updates.log 2>&1 &"); ?>
 	</head>
 	<body>
-		<iframe seamless id="framedisplay" style="width: 100%; height: 100%" src="shellscripts/catfile.php?file=/tmp/sysinfo.cgi.log"></iframe>
+		<iframe seamless id="framedisplay" style="width: 100%; height: 100%" src="shellscripts/catfile.php?file=/tmp/linux-webui_updates.log"></iframe>
 	</body>
 </html>
