488008f1537921564238241e0f74e1e83e4416cb
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 09:49:42 -0400

    Check updates design trial

--- updates.php
+++ updates.php
@@ -10,6 +10,6 @@
 		<?php exec("nohup emerge -pv --update --deep --with-bdeps=y --newuse @world >/tmp/linux-webui_updates.log 2>&1 &"); ?>
 	</head>
 	<body>
-		<iframe seamless id="framedisplay" style="width: 100%; height: 100%" src="shellscripts/catfile.php?file=/tmp/linux-webui_updates.log"></iframe>
+		<iframe seamless id="framedisplay" style="width: 95%; height: 95%" src="shellscripts/catfile.php?file=/tmp/linux-webui_updates.log"></iframe>
 	</body>
 </html>
