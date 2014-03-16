24dd981e784310facc075e2a582d81df0742a006
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 10:53:05 -0400

    Index.php header and footer common

--- index.php
+++ index.php
@@ -6,65 +6,7 @@
 	</head>
     <body>
         <div class="wrapper">
-        	<div class="header">
-        		<table class="header_table">
-        			<tr>
-        				<td id="header_table_data_left">
-        					<a href="."><div class="header_button">
-        						<p>&nbsp;</p>
-        						<h3 class="header_button_text">Info</h3>
-        						<p>&nbsp;</p>
-        					</div></a>
-        					<?php
-        						if (file_exists('services.php')) {
-        					?>
-        							<a href="services.php"><div class="header_button">
-										<p>&nbsp;</p>
-        								<h3 class="header_button_text">Services</h3>
-        								<p>&nbsp;</p>
-        							</div></a>
-        					<?php
-        						}
-        						if (file_exists('storage.php')) {
-        					?>
-        							<a href="storage.php"><div class="header_button">
-										<p>&nbsp;</p>
-        								<h3 class="header_button_text">Storage</h3>
-        								<p>&nbsp;</p>
-        							</div></a>
-        					<?php
-        						}
-        						if (file_exists('users.php')) {
-        					?>
-        							<a href="users.php"><div class="header_button">
-										<p>&nbsp;</p>
-        								<h3 class="header_button_text">Users</h3>
-        								<p>&nbsp;</p>
-        							</div></a>
-        					<?php
-        						}
-        						if (file_exists('web.php')) {
-        					?>
-        							<a href="web.php"><div class="header_button">
-										<p>&nbsp;</p>
-        								<h3 class="header_button_text">Web</h3>
-        								<p>&nbsp;</p>
-        							</div></a>
-        					<?php
-        						}
-        					?>
-        					<div id="header_button_last">
-								<p>&nbsp;</p>
-								<h3>&nbsp;</h3>
-								<p>&nbsp;</p>
-        					</div>
-        				</td>
-        				<td id="header_table_data_right">
-        					<a href="about.php"><h3 id="header_brand_text">linux-webui</h3></a>
-        				</td>
-        			</tr>
-        		</table>
-        	</div>
+        	<?php include("header.php"); ?>
         	<div class="body">
         		<div>&nbsp;</div>
         		<div class="body_content">
@@ -112,8 +54,6 @@
         	</div>
             <div class="push"></div>
         </div>
-        <div class="footer">
-            <p>&copy; 2014 Viraj Chitnis</p>
-        </div>
+        <?php include("footer.php"); ?>
     </body>
 </html>
