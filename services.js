39c4e5aab2202783618bf237d75efafe3c857450
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 17:14:26 -0400

    Gentoo notice design trial I

--- services.js
+++ services.js
@@ -27,6 +27,5 @@ function hideUpdate () {
 
 function showGentoo () {
 	document.getElementById('gentoo_notice').style.display = "block";
-	document.getElementById('gentoo_notice_space').style.display = "block";
 	document.getElementById('update_management').style.display = "none";
 }
