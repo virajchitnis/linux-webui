ffa0345cefcf0afceb0692bd44cec62ee858a2e1
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-15 21:54:35 -0400

    Fixed mismatching variables problem

--- shellscripts/service_operation.php
+++ shellscripts/service_operation.php
@@ -1,8 +1,8 @@
 <?php
-	if ((isset($_GET['service'])) && (isset($_GET['operand']))) {
+	if ((isset($_GET['service'])) && (isset($_GET['operation']))) {
 		$service = $_GET['service'];
-		$operand = $_GET['operand'];
-		exec("sudo service ".$service." ".$operand);
+		$operand = $_GET['operation'];
+		exec("sudo service ".$service." ".$operation);
 	}
 	
 	header('Location ../services.php');
