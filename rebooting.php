d71d57f7e1bbcd8a47bbaac8bad410b99d101615
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 11:26:55 -0400

    Added progress dots to rebooting.php

--- rebooting.php
+++ rebooting.php
@@ -3,10 +3,15 @@
 		<link rel="stylesheet" type="text/css" href="design.css">
 		<link rel="stylesheet" type="text/css" href="about.css">
 		<script type="text/JavaScript">
-			var count = 180;
+			var count = 1;
 			var counter = setInterval(refreshTimer, 1000 * count);
 			function refreshTimer(){
-				window.location.href = 'services.php';
+				if (count == 180) {
+					window.location.href = 'services.php';
+				}
+				else {
+					document.getElementById("progress_dots").innerHTML += ".";
+				}
 			}
 		</script>
 		<title>linux-webui - Linux Server Control Panel</title>
@@ -17,8 +22,7 @@
         	<div class="body">
         		<div>&nbsp;</div>
         		<div class="body_content">
-        			<h3>Rebooting</h3>
-        			<p>The server is rebooting, please wait.....</p>
+        			<p>The server is rebooting, please wait<span id="progress_dots">.</span></p>
         			<p>The page will auto redirect once the rebooting is complete.</p>
         		</div>
         	</div>
