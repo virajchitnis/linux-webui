7d1bde87fc34d17de07f6dde884162bf26b66dc9
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 11:48:34 -0400

    Fixed error that prevented samba memory usage from displaying

--- services.php
+++ services.php
@@ -167,7 +167,7 @@
         						<tr>
         							<td class="body_table_data">Samba</td>
         							<td class="body_table_data"><?php echo exec('sudo service samba status'); ?></td>
-        							<td class="body_table_data"><?php echo exec("sudo pmap $(pidof samba | awk '{print $1}') | tail -1 | awk '{print $2}'"); ?></td>
+        							<td class="body_table_data"><?php echo exec("sudo pmap $(pidof smbd | awk '{print $1}') | tail -1 | awk '{print $2}'"); ?></td>
         							<td class="body_table_data">
         								<?php
         									if ((exec("sudo service samba status | awk '{print $3}'")) == "started") {
