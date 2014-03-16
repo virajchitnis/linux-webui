ee6489cfcf1c7928d92d2d05bca1aa89bb3ae4c1
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-16 11:00:10 -0400

    Fixed wrong css file in about page

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="design.css">
-		<link rel="stylesheet" type="text/css" href="index.css">
+		<link rel="stylesheet" type="text/css" href="about.css">
 		<title>linux-webui - Linux Server Control Panel</title>
 	</head>
     <body>
